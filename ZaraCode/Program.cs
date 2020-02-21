2ca82b81c6b6cc766003d70913d6a1476fbba1eb
giselle <gisellelago25994@gmail.com>
2020-02-21 19:06:34 +0100

    Agregar archivos de proyecto.

---
+++ ZaraCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaraCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataInfo dataInfo = new DataInfo();
+            dataInfo.ExtractData();
+            Console.ReadKey();
+        }
+    }
+}
