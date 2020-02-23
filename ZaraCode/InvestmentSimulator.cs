a3ec5798698f35bd8e7ae139c374e4b5ec451559
giselle <gisellelago25994@gmail.com>
2020-02-23 13:48:59 +0100

    Fix InvestmentSimulator Class.

--- ZaraCode/InvestmentSimulator.cs
+++ ZaraCode/InvestmentSimulator.cs
@@ -18,8 +18,8 @@ namespace ZaraCode
             for (int i = 0; i < dataList.Count; i++)
             {
                 var dailyStock = dataList[i];
-                var dailyFluctuation = (float)Math.Round((dailyStock.CloseDay - dailyStock.OpenDay) * 100 / dailyStock.OpenDay, 3);
-                var fluctuationAfterMarket = (float)Math.Round((dailyStock.OpenDay - cDay) * 100 / cDay, 3);
+                var dailyFluctuation = (float)Math.Round((dailyStock.CloseDay - dailyStock.OpenDay) / dailyStock.OpenDay, 3);
+                
                 var dTime = dailyStock.DateTime;
                 var firstDayOfNextMonth = new DateTime(dTime.Year, dTime.Month, 1).AddMonths(1);
                 int vector = (((int)firstDayOfNextMonth.DayOfWeek + 1) % 7) + 2;
@@ -29,9 +29,10 @@ namespace ZaraCode
                     if (lastThursdayOfMonth)
                     {
                         if (cDay != 0)
-                        { 
-                            currentCapital += (float)Math.Round(dailyFluctuation * currentCapital / 100 , 3);
-                            currentCapital += (float)Math.Round(fluctuationAfterMarket * currentCapital / 100 + monthlyInvestment, 3);
+                        {
+                            var fluctuationAfterMarket = (float)Math.Round((dailyStock.OpenDay - cDay) / cDay, 3);
+                            currentCapital += (float)Math.Round(fluctuationAfterMarket * currentCapital + monthlyInvestment, 3);
+                            currentCapital += (float)Math.Round(dailyFluctuation * currentCapital, 3);
                             lastThursdayOfMonth = false;
                             cDay = dailyStock.CloseDay;
                         }
@@ -40,33 +41,18 @@ namespace ZaraCode
                             if (currentCapital == 0)
                             {
                                 currentCapital = monthlyInvestment;
-                                currentCapital += (float)Math.Round(dailyFluctuation * currentCapital / 100, 3);
+                                currentCapital += (float)Math.Round(dailyFluctuation * currentCapital, 3);
                                 lastThursdayOfMonth = false;
                                 cDay = dailyStock.CloseDay;
                             }
-                            else
-                            {
-                                currentCapital += (float)Math.Round(dailyFluctuation * currentCapital / 100 + monthlyInvestment, 3);
-                                lastThursdayOfMonth = false;
-                                cDay = dailyStock.CloseDay;
-                            }
-                            
-                            
                         } 
                     }
                     else if(init)
                     {
-                        if (cDay != 0)
-                        {
-                            currentCapital += (float)Math.Round(dailyFluctuation * currentCapital / 100, 3);
-                            currentCapital += (float)Math.Round(fluctuationAfterMarket * currentCapital / 100, 3);
-                            cDay = dailyStock.CloseDay;
-                        }
-                        else
-                        {
-                            currentCapital += (float)Math.Round(dailyFluctuation * currentCapital / 100, 3);
-                            cDay = dailyStock.CloseDay;
-                        }   
+                        var fluctuationAfterMarket = (float)Math.Round((dailyStock.OpenDay - cDay) / cDay, 3);
+                        currentCapital += (float)Math.Round(fluctuationAfterMarket * currentCapital, 3);
+                        currentCapital += (float)Math.Round(dailyFluctuation * currentCapital, 3);
+                        cDay = dailyStock.CloseDay; 
                     }
 
                     if (dTime == r)
@@ -74,7 +60,6 @@ namespace ZaraCode
                         lastThursdayOfMonth = true;
                     }
                     init = true;
-
                 }
             }
             var finalCapital = (float)Math.Round(currentCapital, 3);
