4178fcccb73ae6d711ecd331217bd6579f652848
giselle <gisellelago25994@gmail.com>
2020-02-24 23:49:19 +0100

    Create the StopWatch.

--- ZaraCode/Program.cs
+++ ZaraCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ZaraCode
@@ -7,15 +8,22 @@ namespace ZaraCode
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("Running Zara Tech Code...");
             InvestmentSimulator indexSimulator = new InvestmentSimulator();
             ExcelSource excelSource = new ExcelSource();
             ExcelGenerator excelGenerator = new ExcelGenerator();
             var list = excelSource.ExtractData().ToList();
             var resultFinalCapital = indexSimulator.GetFinalCapital(list, 50);
+            Console.WriteLine("Exporting data to Excel...");
             var incomeList = resultFinalCapital.Item2;
             excelGenerator.ExportList(incomeList);
             Console.WriteLine("Result value: {0:N3}", resultFinalCapital.Item1);
-            
+            sw.Stop();
+            Console.WriteLine("Time elapsed: {0}", sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff"));
+
+
             Console.ReadKey();
         }
     }
