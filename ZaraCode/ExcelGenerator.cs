72834ccff3842263e189dabd770f4cb9fbbf73e9
giselle <gisellelago25994@gmail.com>
2020-02-25 01:47:16 +0100

    Fix ExcelGenerator.

--- ZaraCode/ExcelGenerator.cs
+++ ZaraCode/ExcelGenerator.cs
@@ -19,24 +19,9 @@ namespace ZaraCode
                 List<Income> incomes = income;
                 worksheet.ImportData(incomes, 2, 1, false);
 
-                IStyle tableHeader = workbook.Styles.Add("TableHeaderStyle");
-                tableHeader.Font.Color = ExcelKnownColors.Dark_green;
-                tableHeader.Font.Bold = true;
-                tableHeader.Font.Size = 11;
-                tableHeader.Font.FontName = "Calibri";
-                tableHeader.HorizontalAlignment = ExcelHAlign.HAlignCenter;
-                tableHeader.VerticalAlignment = ExcelVAlign.VAlignCenter;
-                tableHeader.Borders[ExcelBordersIndex.EdgeLeft].LineStyle = ExcelLineStyle.Thin;
-                tableHeader.Borders[ExcelBordersIndex.EdgeRight].LineStyle = ExcelLineStyle.Thin;
-                tableHeader.Borders[ExcelBordersIndex.EdgeTop].LineStyle = ExcelLineStyle.Thin;
-                tableHeader.Borders[ExcelBordersIndex.EdgeBottom].LineStyle = ExcelLineStyle.Thin;
-
                 worksheet["A1"].Text = "Date";
-                worksheet["A1"].CellStyle = tableHeader;
                 worksheet["B1"].Text = "Income";
-                worksheet["B1"].CellStyle = tableHeader;
-               
-
+ 
                 worksheet.UsedRange.AutofitColumns();
                 Stream excelStream = File.Create(Path.GetFullPath(@"ZaraCode.xlsx"));
                 workbook.SaveAs(excelStream);
